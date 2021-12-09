8f6f2b50d7a81be7e4bf7d52557e3d41a169d772
Yao Song <84160498+YaoS-Code@users.noreply.github.com>
2021-12-08 20:06:36 -0800

    change header to dropdown menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import "./Header.css"
+import { Dropdown, ButtonGroup, Button } from 'react-bootstrap';
+// import About from '../About/About'
 function Header() {
   return (
     <div>
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Borui
-        </a>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark ">
+      <div className="container-fluid header-container">
+        <Button className="bg-dark logo-container" href="/">
+          <img id="logo" src={"./images/logo.png"}/>
+        </Button>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,29 +23,75 @@ function Header() {
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/">
+              {/* <a className="nav-link" aria-current="page" href="/">
                 HOME 主页
-              </a>
+              </a> */}
+              <Dropdown as={ButtonGroup}>
+                  <a href="/">
+                  <Button className="button-header" variant="info">HOME 主页</Button>
+                  </a>
+                  {/* <Dropdown.Toggle split variant="info" id="dropdown-split-info" /> */}
+                  {/* <Dropdown.Menu>
+                    <Dropdown.Item href="#/drawings">Hangman</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Flappy Bird</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Other</Dropdown.Item>
+                  </Dropdown.Menu> */}
+                </Dropdown>  
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/game">
-                GAME 游戏
-              </a>
+              <Dropdown as={ButtonGroup}>
+                  <a href="/game">
+                  <Button className="button-header" variant="info">GAME 游戏</Button>
+                  </a>
+                  <Dropdown.Toggle split variant="info" id="dropdown-split-info" />
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/drawings">Hangman</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Flappy Bird</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Other</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>  
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/course">
-                COURSE 课程
-              </a>
+              <Dropdown as={ButtonGroup}>
+                  <a href="/course">
+                  <Button className="button-header" variant="info">COURSE 课程</Button>
+                  </a>
+                  <Dropdown.Toggle split variant="info" id="dropdown-split-info" />
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/drawings">Coding 编程</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Math 数学</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">English 英语</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/arts">
-                ART 艺术
-              </a>
+                <Dropdown as={ButtonGroup}>
+                  <a href="/arts">
+                  <Button className="button-header" variant="info">ART 艺术</Button>
+                  </a>
+                  <Dropdown.Toggle split variant="info" id="dropdown-split-info" />
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/drawings">Drawings 绘画</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Piano 钢琴</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Cello 大提琴</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">
+              {/* <a className="nav-link" href="/about">
                 ABOUT 关于
-              </a>
+              </a> */}
+              <Dropdown as={ButtonGroup}>
+                  <a href="/about">
+                  <Button className="button-header" variant="info"> ABOUT 关于</Button>
+                  </a>
+                  {/* <Dropdown.Toggle split variant="info" id="dropdown-split-info" /> */}
+                  {/* <Dropdown.Menu>
+                    <Dropdown.Item href="#/drawings">Drawings 绘画</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Piano 钢琴</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Cello 大提琴</Dropdown.Item>
+                  </Dropdown.Menu> */}
+                </Dropdown>
             </li>
           </ul>
           <form className="d-flex">
@@ -53,12 +101,34 @@ function Header() {
               placeholder="Search"
               aria-label="Search"
             /> */}
-            <button className="btn btn-outline-primary" type="submit">
+            <Dropdown as={ButtonGroup}>
+                <a href="/about">
+                <Button className="button-header" variant="warning" type="submit"> Login 登录</Button>
+                </a>
+                {/* <Dropdown.Toggle split variant="info" id="dropdown-split-info" /> */}
+                {/* <Dropdown.Menu>
+                  <Dropdown.Item href="#/drawings">Drawings 绘画</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Piano 钢琴</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Cello 大提琴</Dropdown.Item>
+                </Dropdown.Menu> */}
+              </Dropdown>
+              <Dropdown as={ButtonGroup}>
+                <a href="/about">
+                <Button className="button-header" variant="success" type="submit"> Register 注册</Button>
+                </a>
+                {/* <Dropdown.Toggle split variant="info" id="dropdown-split-info" /> */}
+                {/* <Dropdown.Menu>
+                  <Dropdown.Item href="#/drawings">Drawings 绘画</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Piano 钢琴</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Cello 大提琴</Dropdown.Item>
+                </Dropdown.Menu> */}
+              </Dropdown>
+            {/* <button className="btn btn-outline-primary" type="submit">
               Register
             </button>
             <button className="btn btn-outline-success" type="submit">
               Login
-            </button>
+            </button> */}
           </form>
         </div>
       </div>
